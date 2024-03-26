d7322266b505fe1700a5afcea47f2c6657ee7adc
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-03-26 18:59:44 -0400

    Update global-nav.php

--- global-nav.php
+++ global-nav.php
@@ -2,15 +2,15 @@
 			<!-- STEP 1: Add a nested UL and LI elements for the two pages inside the "About" section, privacy-policy.php and terms-of-use.php -->
 			<nav>
 				<ul>
-					<li><a href="/lab-11-start/index.php" title="Go to the Home page">Home</a></li>
-					<li><a href="/lab-11-start/red.php" title="Learn about red blocks">Red Blocks</a></li>
-					<li><a href="/lab-11-start/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
-					<li><a href="/lab-11-start/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
-					<li><a href="/lab-11-start/green.php" title="Learn about green blocks">Green Blocks</a></li>
-					<li><a href="/lab-11-start/about.php" title="Learn more about us">About</a>
+					<li><a href="index.php" title="Go to the Home page">Home</a></li>
+					<li><a href="red.php" title="Learn about red blocks">Red Blocks</a></li>
+					<li><a href="blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+					<li><a href="yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+					<li><a href="green.php" title="Learn about green blocks">Green Blocks</a></li>
+					<li><a href="about.php" title="Learn more about us">About</a>
 						<ul>
-							<li><a href="/lab-11-start/about/privacy-policy.php" title="Learn about our privacy policy">Privacy Policy</a></li>
-							<li><a href="/lab-11-start/about/terms-of-use.php" title="Learn about our terms of use">Term of Use</a></li>
+							<li><a href="privacy-policy.php" title="Learn about our privacy policy">Privacy Policy</a></li>
+							<li><a href="terms-of-use.php" title="Learn about our terms of use">Term of Use</a></li>
 						</ul>
 					</li>
 				</ul>
